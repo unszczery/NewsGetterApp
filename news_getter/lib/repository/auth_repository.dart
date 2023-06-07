b975bc1889178ea2b0f93abef3aaf7137483864a
ThouSelf <sasatokoi@gmail.com>
2023-06-07 23:16:02 +0300

    AuthRepository initial

---
+++ news_getter/lib/repository/auth_repository.dart
@@ -0,0 +1,70 @@
+
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthRepository {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> singInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> signUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw Exception('Provided password is too weak');
+      } else if (e.code == 'email-already-in-use') {
+        throw Exception('Account already exists');
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        throw Exception('User with this email is not found');
+      } else if (e.code == 'wrong-password') {
+        throw Exception('Wrong password is provided');
+      }
+    }
+  }
+
+  Future<void> signOut(){
+    try{
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      throw Exception(e.toString);
+   }   
+    }
+
+
+}
